68a56b7613aee79a4a322f44abf1078689102f28
KatyaM8 <teken2004@mail.ru>
2024-04-29 13:55:00 +0300

    katya finished questions

--- src/Content/questions.js
+++ src/Content/questions.js
@@ -17,14 +17,14 @@ const data = {
                 validAnswer: 1,
             },
             {
-                question: "Какой персонаж скандинавской мифологии был известен своими путешествиями по морю?",
-                answers: ["Рагнарок", "Один", "Эйрик Блутакса", "Локи"],
-                validAnswer: 3,
+                question: "We ___ dinner before our parents ___",
+                answers: ["had had, arrived", "had, arrived", "had, had arrived", "had, arrive"],
+                validAnswer: 1,
             },
             {
-                question: "Кто является отцом богов в скандинавской мифологии?",
-                answers: ["Локи", "Один", "Тор", "Браги"],
-                validAnswer: 2,
+                question: "They told me that he ___ in this company for more than 30 years!",
+                answers: ["worked", "had worked", "has been working", "had been working"],
+                validAnswer: 4,
             },
             {
                 question: "We ___ tea when our grandparents came.",
@@ -32,9 +32,9 @@ const data = {
                 validAnswer: 1,
             },
             {
-                question: "Кто является богиней любви в скандинавской мифологии?",
-                answers: ["Фрейя", "Скальд", "Сиф", "Хела"],
-                validAnswer: 1,
+                question: "I ___ my project by 3 o’clock yesterday.",
+                answers: ["had finish", "had finished", "finished", "finish"],
+                validAnswer: 2,
             },
             {
                 question: "It was my brother`s birthday on Sunday, so I ___ him a postcard.",
@@ -42,9 +42,9 @@ const data = {
                 validAnswer: 1,
             },
             {
-                question: "Какое животное является символом скандинавской мифологии и часто изображается на викингских кораблях?",
-                answers: ["Белый медведь", "Волк", "Дракон", "Драккар"],
-                validAnswer: 3,
+                question: "What ___ ? You looked so tired.",
+                answers: ["had been you doing", "had you been doing", "had been doing you", "you had been doing"],
+                validAnswer: 2,
             },
             {
                 question: "We ___ last night due to the nasty weather.",
@@ -57,14 +57,14 @@ const data = {
                 validAnswer: 3,
             },
             {
-                question: "Как называется мир, в котором пребывают погибшие в битвах в скандинавской мифологии?",
-                answers: ["Асгард", "Мидгард", "Валгалла", "Йотунхейм"],
+                question: "I ___ out that I ___ the exam.",
+                answers: ["had found, didn’t pass", "had found, hadn’t passed", "found, hadn’t passed", "had founded, didn`t pass"],
                 validAnswer: 3,
             },
             {
-                question: "Кто из следующих богов скандинавской мифологии является богом мудрости, поэзии и вдохновения?",
-                answers: ["Один", "Тор", "Фрейя", "Бали"],
-                validAnswer: 1,
+                question: "I ___ in a flat for 30 years before I moved to a house.",
+                answers: ["had live", "Тhad been living", "had lived", "was living"],
+                validAnswer: 2,
             },
             {
                 question: "Mary ___ home when she met her old friend.",
@@ -72,167 +72,182 @@ const data = {
                 validAnswer: 3,
             },
             {
-                question: "Какой герой скандинавской мифологии был известен своим могучим молотом, который мог разрушить все вокруг?",
-                answers: ["Тор", "Бальдр", "Хель", "Йормунганд"],
-                validAnswer: 1,
+                question: "He ___ it secret that he ___ a bicycle for his son.",
+                answers: ["keep, had bought", "kept, had bought", "kept, bought", "keeped, had bought"],
+                validAnswer: 2,
+            },
+            {
+                question: "It ___ all night so I had to dig out my car in the morning.",
+                answers: ["had snowed", "has been snowing", "was snowing", "had been snowing"],
+                validAnswer: 4,
             },
         ]
     },
     present: {
         questions: [
             {
-                question: "Какое животное помогло Гераклу украсть яблоки Гесперид?",
-                answers: ["Конь", "Пегас", "Серебряный бык", "Олень"],
+                question: "They ___ orange juice.",
+                answers: ["drinks", "doesn`t drink", "doesn`t drinks", "drink"],
                 validAnswer: 4,
             },
             {
-                question: "Кто был первым героем, который умер в Троянской войне?",
-                answers: ["Ахиллес", "Хризес", "Диомед", "Патрокл"],
-                validAnswer: 2,
-            },
-            {
-                question: "Как звали бога ветра и грозы в греческой мифологии?",
-                answers: ["Посейдон", "Зевс", "Геракл", "Арес"],
-                validAnswer: 2,
-            },
-            {
-                question: "Как называлась женщина, влюбленная в своего сына Адонаиса?",
-                answers: ["Антиопа", "Бризейда", "Мира", "Афродита"],
-                validAnswer: 4,
-            },
-            {
-                question: "Как звали героя, который смог украсть огонь из Олимпа?",
-                answers: ["Пандора", "Прометей", "Гермес", "Геракл"],
-                validAnswer: 2,
-            },
-            {
-                question: "Как звали спутника Персея, который помог ему убить Медузу?",
-                answers: ["Кассиопея", "Андромеда", "Пегас", "Ариадна"],
-                validAnswer: 4,
-            },
-            {
-                question: "Как звали богиню охоты в греческой мифологии?",
-                answers: ["Афина", "Гера", "Деметра", "Артемида"],
-                validAnswer: 4,
-            },
-            {
-                question: "Как звали бога огня и кузнечного мастерства в греческой мифологии?",
-                answers: ["Гефест", "Аполлон", "Арес", "Гермес"],
+                question: "I ___ for Christine. Do you know where she is?",
+                answers: ["am looking", "are looking", "is looking", "look"],
                 validAnswer: 1,
             },
             {
-                question: "Как звали бога подземного мира в греческой мифологии?",
-                answers: ["Аид", "Посейдон", "Гермес", "Арес"],
-                validAnswer: 1,
-            },
-            {
-                question: "Как звали бога света, музыки и поэзии в греческой мифологии?",
-                answers: ["Гермес", "Аполлон", "Арес", "Дионис"],
-                validAnswer: 2,
-            },
-            {
-                question: "Как звали богиню любви и красоты в греческой мифологии?",
-                answers: ["Деметра", "Гестия", "Артемида", "Афродита"],
-                validAnswer: 4,
-            },
-            {
-                question: "Как звали бога войны в греческой мифологии?",
-                answers: ["Геракл", "Арес", "Гермес", "Аполлон"],
-                validAnswer: 2,
-            },
-            {
-                question: "Как звали бога моря в греческой мифологии?",
-                answers: ["Прометей", "Посейдон", "Геракл", "Гермес"],
-                validAnswer: 2,
-            },
-            {
-                question: "Кто была богиней мудрости?",
-                answers: ["Гера", "Афина", "Артемида", "Афродита"],
-                validAnswer: 2,
-            },
-            {
-                question: "Кто был богом моря и землетрясений?",
-                answers: ["Аид", "Дионис", "Посейдон", "Геракл"],
+                question: "How long ___ singing lessons?",
+                answers: ["you have been taking", "has you been taking", "have you been taking", "been have you taking"],
                 validAnswer: 3,
+            },
+            {
+                question: "Nick ___ a live football match before.",
+                answers: ["have never seen", "has never seen", "hasn’t never seen", "has seen never"],
+                validAnswer: 2,
+            },
+            {
+                question: "It ___ dark. Shall I turn on the light?",
+                answers: ["are getting", "am getting", "get", "is getting"],
+                validAnswer: 4,
+            },
+            {
+                question: "A pianist ___ the piano.",
+                answers: ["play", "plays", "doesn`t plays", "don`t play"],
+                validAnswer: 2,
+            },
+            {
+                question: "Lena ___ geometry since she was 30.",
+                answers: ["has been teaching", "has teaching", "teaches", "have been"],
+                validAnswer: 1,
+            },
+            {
+                question: "Have you got an umbrella? It ___ to rain.",
+                answers: ["is starting", "starts", "am starting", "are starting"],
+                validAnswer: 1,
+            },
+            {
+                question: "My wife ___ invitations for the party.",
+                answers: ["have already sent", "has already sent", "has already send", "has sent already"],
+                validAnswer: 2,
+            },
+            {
+                question: "Mary and Sue ___ to the cinema.",
+                answers: ["goes", "doesn`t go", "doesn`t goes", "don`t go"],
+                validAnswer: 4,
+            },
+            {
+                question: "Donna ___ for a job for half a year.",
+                answers: ["has looking", "have been looking", "look", "has been looking"],
+                validAnswer: 4,
+            },
+            {
+                question: "Why are all these people here? What ___ ?",
+                answers: ["are happening", "is happening", "am happening", "happens"],
+                validAnswer: 2,
+            },
+            {
+                question: "Are you going anywhere this summer? Yes, we ___ a trip to Italy.",
+                answers: ["have book", "has booked", "have booked", "book"],
+                validAnswer: 3,
+            },
+            {
+                question: "Mark ... cakes.",
+                answers: ["don't love", "doesn't love", "love", "doesn't loves"],
+                validAnswer: 2,
+            },
+            {
+                question: "Brian ___ his new project yet.",
+                answers: ["not started", "haven’t started", "hasn’t started", "doesn`t start"],
+                validAnswer: 3,
+            },
+            {
+                question: "The baby ___ for three hours.",
+                answers: ["been sleeping", "has been sleeping", "have been sleeping", "sleeps"],
+                validAnswer: 2,
             },
         ]
     },
     future: {
         questions: [
             {
-                question: "Египетская богиня с причудливой головой льва?",
-                answers: ["Исис", "Нефертум", "Секхмет", "Хатхор"],
-                validAnswer: 3,
-            },
-            {
-                question: "Каким прозвищем называли египетскую богиню неба Нут?",
-                answers: ["Владычица бесконечности", "Королева вселенной", "Хранитель небесных тел", "Матерь звезд"],
-                validAnswer: 1,
-            },
-            {
-                question: "Как звали богиню защиты и домашнего очага в египетской мифологии?",
-                answers: ["Сехмет", "Маат", "Бастет", "Нефертум"],
-                validAnswer: 3,
-            },
-            {
-                question: "Как звали бога смерти и тьмы в египетской мифологии?",
-                answers: ["Анубис", "Озирис", "Сет", "Хор"],
-                validAnswer: 3,
-            },
-            {
-                question: "Какая из египетских богинь была покровительницей материнства?",
-                answers: ["Бастет", "Хатхор", "Иштар", "Мут"],
+                question: "He ___ to the cinema tomorrow.",
+                answers: ["go not will", "will not go", "will go not", "go"],
                 validAnswer: 2,
             },
             {
-                question: "Какое животное является символом магических свойств и бесконечности?",
-                answers: ["Скорпион", "Жук-некрофаг", "Кальмар", "Змея"],
-                validAnswer: 4,
-            },
-            {
-                question: "Как называется миф, описывающий борьбу Ра с Апопом, стоящим на пути солнца?",
-                answers: ["О Тифоне", "Об Изиде и Озирисе", "Об Уаджет-глазе", "Об Апопи"],
-                validAnswer: 4,
-            },
-            {
-                question: "Как звали бога жизни и плодородия в египетской мифологии?",
-                answers: ["Озирис", "Сет", "Хор", "Себек"],
-                validAnswer: 4,
-            },
-            {
-                question: "Как звали богиню неба в египетской мифологии?",
-                answers: ["Ра", "Нут", "Бастет", "Сехмет"],
-                validAnswer: 2,
-            },
-            {
-                question: "Как звали бога мертвых в египетской мифологии?",
-                answers: ["Анубис", "Ра", "Озирис", "Сет"],
+                question: "В каком варианте глагол to stay правильно оформлен во времени Future Perfect Continuous?",
+                answers: ["will have been staying", "will have being staying", "will have being stayed", "will have been stay"],
                 validAnswer: 1,
             },
             {
-                question: "Как звали богиню материнства и плодородия в египетской мифологии?",
-                answers: ["Сехмет", "Бастет", "Маат", "Изида"],
-                validAnswer: 4,
-            },
-            {
-                question: "Кто был главным богом в египетской мифологии?",
-                answers: ["Анубис", "Ра", "Озирис", "Хор"],
-                validAnswer: 2,
-            },
-            {
-                question: "Какое животное было священным символом богини Исиды?",
-                answers: ["Крокодил", "Сокол", "Кошка", "Кролик"],
-                validAnswer: 4,
-            },
-            {
-                question: "Какой бог был символом солнца и создателем мира?",
-                answers: ["Ра", "Тот", "Хор", "Озирис"],
-                validAnswer: 1,
-            },
-            {
-                question: "Как назывался бог баланса и справедливости?",
-                answers: ["Гор", "Анубис", "Маат", "Амон"],
+                question: "I ___ this book by next week.",
+                answers: ["will not read", "won’t had read", "will not have read", "will no had read"],
                 validAnswer: 3,
+            },
+            {
+                question: "What ___ after work?",
+                answers: ["will he do", "do will he", "do", "he will do"],
+                validAnswer: 1,
+            },
+            {
+                question: " I ___ during rush hour.",
+                answers: ["will be driving", "will have drive", "will be drive", "will have driving"],
+                validAnswer: 1,
+            },
+            {
+                question: "At the end of this school year, she ____ for 51 years. ",
+                answers: ["will teaching", "will have teaching", "will been teaching", "will have been teaching"],
+                validAnswer: 4,
+            },
+            {
+                question: "At noon tomorrow, I ________ on a beach somewhere.",
+                answers: ["'ll be relaxing", "relax", "relax", "will being relax"],
+                validAnswer: 1,
+            },
+            {
+                question: "Tennis fans ___ at Wimbledon all day to buy tickets.",
+                answers: ["will have being queueing", "will have been queue", "will been have queueing", "will have been queueing"],
+                validAnswer: 4,
+            },
+            {
+                question: "He will not be ___ the bus today.",
+                answers: ["take", "taken", "take", "taking"],
+                validAnswer: 4,
+            },
+            {
+                question: "I ___ all my exams by 15 June. ",
+                answers: ["will have pass", "will have passed", "will pass", "will passed"],
+                validAnswer: 2,
+            },
+            {
+                question: "We ___ an English lesson on Friday.",
+                answers: ["have", "not will have", "will have not", "won’t have"],
+                validAnswer: 4,
+            },
+            {
+                question: "He ___ playing football by 3 o’clock tomorrow.",
+                answers: ["will have finished", "will finish", "will has finished", "will finish"],
+                validAnswer: 1,
+            },
+            {
+                question: "Where ___ sleeping?",
+                answers: ["you be", "will you", "will you be", "you will"],
+                validAnswer: 3,
+            },
+            {
+                question: "Alice ___ dinner by 7 o’clock tomorrow. ",
+                answers: ["will have cooked", "will has cooked", "will cooked", "will cook"],
+                validAnswer: 1,
+            },
+            {
+                question: "She ___ the movie tonight.",
+                answers: ["watch will", "will watch", "watch’ll", "watch"],
+                validAnswer: 2,
+            },
+            {
+                question: "Next year I ___ in the company for 30 years.",
+                answers: ["will have being working", "will have been working", "will have working", "will have work"],
+                validAnswer: 2,
             },
         ]
     }
