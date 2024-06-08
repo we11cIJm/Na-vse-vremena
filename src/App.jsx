ab497a3320eaf0c2048c3eb3ae79bc47fb68a6ba
Nikolay <n0.samsonov29@gmail.com>
2024-06-08 14:40:59 +0300

    commented mine version, written Katya's version

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,251 @@
+// import './App.css';
+// import Menu from './Components/Menu';
+// import toStartQuiz from './Components/Menu';
+// import showTheory from './Components/Menu';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   useNavigate,
+//   Routes,
+//   Route,
+//   useLocation
+// } from "react-router-dom";
+// import Game from './Components/Game';
+// import PastTheory from './Components/PastTheory';
+// import PresentTheory from './Components/PresentTheory';
+// import FutureTheory from './Components/FutureTheory';
+// //import Theory from './Components/Theory';
+// import data from "./Content/questions"
+// import React, {
+//   useReducer,
+//   useState,
+//   useRef,
+//   useEffect,
+// } from "react";
+// import { Button } from "@salutejs/plasma-ui";
+// import { createAssistant, createSmartappDebugger } from '@salutejs/client';
+// import { darkJoy, darkEva, darkSber } from '@salutejs/plasma-tokens/themes';
+// import { text, background, gradient } from '@salutejs/plasma-tokens';
+// import { createGlobalStyle } from 'styled-components';
+// import {useSpatnavInitialization, useSection} from '@salutejs/spatial';
+//
+// const ThemeBackgroundEva = createGlobalStyle(darkEva);
+// const ThemeBackgroundSber = createGlobalStyle(darkSber);
+// const ThemeBackgroundJoy = createGlobalStyle(darkJoy);
+//
+//
+// const DocStyles = createGlobalStyle`
+//   html {
+//     color: ${text};
+//     background-color: ${background};
+//     background-image: ${gradient};
+//     min-height: 100vh;
+//   }
+// `;
+//
+//
+// function App() {
+//
+//   const [character, setCharacter] = useState('sber');
+//   const [page, setPage] = useState(0);
+//   const navigate = useNavigate();
+//   const initialize = (getState) => {
+//     if (process.env.NODE_ENV === "development") {
+//
+//       return createSmartappDebugger({
+//         token: process.env.REACT_APP_TOKEN ?? "",
+//         initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
+//         getState,
+//
+//       });
+//     }
+//     return createAssistant({ getState });
+//   };
+//
+//
+//   const childRef = useRef(null);
+//   const assistantStateRef = useRef();
+//   const assistantRef = useRef();
+//   useSpatnavInitialization();
+//
+//   useEffect(() => {
+//     navigate('/');
+//     assistantRef.current = initialize(() => assistantStateRef.current);
+//
+//     assistantRef.current.on("data", (action) => {
+//       handleAssistantDataEvent(action);
+//     });
+//
+//
+//     assistantRef.current.on("command", (event) => {
+//       dispatchAssistantAction(event?.command);
+//     })
+//   }, []);
+//
+//   const handleAssistantDataEventSmartAppData = (event) => {
+//     console.log('AssistantWrapper.handleAssistantEventSmartAppData: event:', event);
+//
+//
+//     if (event.sub !== undefined) {
+//       // this.emit('event-sub', event.sub);
+//       // /*await*/ this._App.handleAssistantSub(event.sub);
+//     }
+//
+//     const action = event.action;
+//     dispatchAssistantAction(action);
+//
+//   }
+//
+//   const dispatchAssistantAction = (action) => {
+//     if (!action) return;
+//
+//     switch (action.type) { //action.types
+//       case 'new_topic': {
+//         console.log('new_topic', action.note)
+//         // navigate('/game', { state: { Type: Number(action.note) } });
+//         navigate('/game', { state: { Type: action.note } });
+//         break;
+//       }
+//       case 'choose_theory': {
+//         console.log('choose_theory', action.note)
+//         // navigate('/theory/'.concat(action.note), { state: { Type: Number(action.note) } });
+//         navigate('/theory/'.concat(action.note), { state: { Type: action.note } });
+//         break;
+//       }
+//
+//       case 'answer':
+//         if (childRef?.current)
+//           // childRef.current.callMethod('ans', Number(action.note));
+//           childRef.current.callMethod('ans', action.note);
+//         break;
+//       case 'next':
+//         if (childRef?.current)
+//           childRef.current.callMethod('next');
+//         break;
+//       case 'menu':
+//         assistantRef.current.sendData({
+//           action: {
+//             action_id: 'list_of_tense',
+//             parameters: {
+//               number: null
+//             }
+//           }
+//         })
+//         navigate('/');
+//         break;
+//
+//       default:
+//       // console.warn('dispatchAssistantAction: Unknown action.type:', action.type)
+//     }
+//   }
+//
+//   const handleAssistantDataEvent = (event) => {
+//     switch (event?.type) {
+//       case "character":
+//         // notify(event.type);
+//         setCharacter(event.character?.id)
+//         break;
+//       case "sdk_answer":
+//         // notify(event.type);
+//         handleAssistantDataEventSmartAppData(event);
+//         break;
+//
+//       case "smart_app_data":
+//         // notify(event.type);
+//         handleAssistantDataEventSmartAppData(event);
+//         break;
+//
+//       default:
+//         break
+//     }
+//   }
+//
+//   function assistant_global(command, type) {
+//     console.log(command, type);
+//     assistantRef.current.sendData({
+//       action: {
+//         action_id: type,
+//         parameters: {
+//           command
+//         }
+//       }
+//     });
+//
+//     if (type === "choose_theme") {
+//     // if (type === 'new_topic') {
+//       switch (command) {
+//         case 1:
+//         // case 'прошедшее':
+//           navigate('/game', { state: { Type: 1 } });
+//           break;
+//         case 2:
+//         // case 'настоящее':
+//           navigate('/game', { state: { Type: 2 } });
+//           break;
+//         case 3:
+//         // case 'будущее':
+//           navigate('/game', { state: { Type: 3 } });
+//           break;
+//         default:
+//           break;
+//       }
+//     } else if (type === 'choose_theory') {
+//       switch (command) {
+//         case 1:
+//         // case 'прошедшее':
+//           navigate('/theory/1', { state: { Type: 1 } });
+//           break;
+//         case 2:
+//         // case 'настоящее':
+//           navigate('/theory/2', { state: { Type: 2 } });
+//           break;
+//         case 3:
+//         // case 'будущее':
+//           // navigate('/theory', { state: { Type: 3 } });
+//           navigate('/theory/3', { state: { Type: 3 } });
+//           break;
+//         default:
+//           break;
+//       }
+//     }
+//   }
+//
+//   return (
+//     <div className="App">
+//       <DocStyles />
+//       {(() => {
+//         switch (character) {
+//           case 'sber':
+//             return <ThemeBackgroundSber />;
+//           case 'eva':
+//             return <ThemeBackgroundEva />;
+//           case 'joy':
+//             return <ThemeBackgroundJoy />;
+//           default:
+//             return;
+//         }
+//       })()}
+//       <Routes>
+//         <Route exact path="/" element={<Menu assistant_global={assistant_global} />} />
+//         <Route exact path="/game" element={<Game ref={childRef} assistant_global={assistant_global} />} />
+//         {/* <Route exact path="/theory" element={<Theory assistant_global={assistant_global} />} /> */}
+//
+//         <Route path="/theory/1" element={<PastTheory assistant_global={assistant_global} />} />
+//         <Route path="/theory/2" element={<PresentTheory assistant_global={assistant_global} />} />
+//         <Route path="/theory/3" element={<FutureTheory assistant_global={assistant_global} />} />
+//       </Routes>
+//     </div>
+//   );
+//
+// }
+//
+// export default App;
+
 import './App.css';
 import Menu from './Components/Menu';
 import toStartQuiz from './Components/Menu';
 import showTheory from './Components/Menu';
+import {useSpatnavInitialization, useSection} from '@salutejs/spatial';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -27,59 +271,42 @@ import { createAssistant, createSmartappDebugger } from '@salutejs/client';
 import { darkJoy, darkEva, darkSber } from '@salutejs/plasma-tokens/themes';
 import { text, background, gradient } from '@salutejs/plasma-tokens';
 import { createGlobalStyle } from 'styled-components';
-import {useSpatnavInitialization, useSection} from '@salutejs/spatial';
 
 const ThemeBackgroundEva = createGlobalStyle(darkEva);
 const ThemeBackgroundSber = createGlobalStyle(darkSber);
 const ThemeBackgroundJoy = createGlobalStyle(darkJoy);
 
 
+
 const DocStyles = createGlobalStyle`
-  html {
+html {
     color: ${text};
     background-color: ${background};
     background-image: ${gradient};
     min-height: 100vh;
-  }
+}
 `;
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Menu />,
-//   },
-//   {
-//     path: "/quizStart",
-//     element: <QuizStart />
-//   },
-//   {
-//     path: "/game",
-//     element: <Game />
-//   }
-// ]);
 
 function App() {
-  /*document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     let currentDate = document.querySelector("theory");
     // rest of your code goes here
-  });*/
+  });
 
   const [character, setCharacter] = useState('sber');
   const [page, setPage] = useState(0);
   const navigate = useNavigate();
   const initialize = (getState) => {
     if (process.env.NODE_ENV === "development") {
-
       return createSmartappDebugger({
         token: process.env.REACT_APP_TOKEN ?? "",
         initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
         getState,
-
       });
     }
     return createAssistant({ getState });
   };
-
 
   const childRef = useRef(null);
   const assistantStateRef = useRef();
@@ -110,7 +337,6 @@ function App() {
   const handleAssistantDataEventSmartAppData = (event) => {
     console.log('AssistantWrapper.handleAssistantEventSmartAppData: event:', event);
 
-
     if (event.sub !== undefined) {
       // this.emit('event-sub', event.sub);
       // /*await*/ this._App.handleAssistantSub(event.sub);
@@ -118,30 +344,29 @@ function App() {
 
     const action = event.action;
     dispatchAssistantAction(action);
-
   }
 
   const dispatchAssistantAction = (action) => {
+    // notify(`action ${action} `);
     if (!action) return;
 
     switch (action.type) { //action.types
       case 'new_topic': {
         console.log('new_topic', action.note)
-        // navigate('/game', { state: { Type: Number(action.note) } });
-        navigate('/game', { state: { Type: action.note } });
+        navigate('/game', { state: { Type: Number(action.note) } });
+        //navigate('/quizStart', { state: { Type: action.note } });
         break;
       }
       case 'choose_theory': {
         console.log('choose_theory', action.note)
-        // navigate('/theory/'.concat(action.note), { state: { Type: Number(action.note) } });
-        navigate('/theory/'.concat(action.note), { state: { Type: action.note } });
+        navigate('/theory/'.concat(action.note), { state: { Type: Number(action.note) } });
+        //navigate('/quizStart', { state: { Type: action.note } });
         break;
       }
 
       case 'answer':
         if (childRef?.current)
-          // childRef.current.callMethod('ans', Number(action.note));
-          childRef.current.callMethod('ans', action.note);
+          childRef.current.callMethod('ans', Number(action.note));
         break;
       case 'next':
         if (childRef?.current)
@@ -160,7 +385,7 @@ function App() {
         break;
 
       default:
-      // console.warn('dispatchAssistantAction: Unknown action.type:', action.type)
+        // console.warn('dispatchAssistantAction: Unknown action.type:', action.type)
     }
   }
 
@@ -196,38 +421,31 @@ function App() {
       }
     });
 
-    if (type === "choose_theme") {
-    // if (type === 'new_topic') {
+    if (type === "choose_theme") { //"new_topic") {
       switch (command) {
-        case 1:
-        // case 'прошедшее':
+        case 1: // "Past practice":
           navigate('/game', { state: { Type: 1 } });
           break;
-        case 2:
-        // case 'настоящее':
+        case 2: // "Present practice":
           navigate('/game', { state: { Type: 2 } });
           break;
-        case 3:
-        // case 'будущее':
+        case 3: // "Future practice":
           navigate('/game', { state: { Type: 3 } });
           break;
         default:
           break;
       }
-    } else if (type === 'choose_theory') {
+    } else if (type === "choose_theory") {
       switch (command) {
-        case 1:
-        // case 'прошедшее':
-          navigate('/theory/1', { state: { Type: 1 } });
+        case 1: //"Past theory":
+          navigate('/theory/'.concat(command), { state: { Type: 1 } });
           break;
-        case 2:
-        // case 'настоящее':
-          navigate('/theory/2', { state: { Type: 2 } });
+        case 2: // "Present theory":
+          navigate('/theory/'.concat(command), { state: { Type: 2 } });
           break;
-        case 3:
-        // case 'будущее':
+        case 3: // "Future theory":
           // navigate('/theory', { state: { Type: 3 } });
-          navigate('/theory/3', { state: { Type: 3 } });
+          navigate('/theory/'.concat(command), { state: { Type: 3 } });
           break;
         default:
           break;
@@ -236,32 +454,31 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <DocStyles />
-      {(() => {
-        switch (character) {
-          case 'sber':
-            return <ThemeBackgroundSber />;
-          case 'eva':
-            return <ThemeBackgroundEva />;
-          case 'joy':
-            return <ThemeBackgroundJoy />;
-          default:
-            return;
-        }
-      })()}
-      <Routes>
-        <Route exact path="/" element={<Menu assistant_global={assistant_global} />} />
-        <Route exact path="/game" element={<Game ref={childRef} assistant_global={assistant_global} />} />
-        {/* <Route exact path="/theory" element={<Theory assistant_global={assistant_global} />} /> */}
+      <div className="App">
+        <DocStyles />
+        {(() => {
+          switch (character) {
+            case 'sber':
+              return <ThemeBackgroundSber />;
+            case 'eva':
+              return <ThemeBackgroundEva />;
+            case 'joy':
+              return <ThemeBackgroundJoy />;
+            default:
+              return;
+          }
+        })()}
+        <Routes>
+          <Route exact path="/" element={<Menu assistant_global={assistant_global} />} />
+          <Route exact path="/game" element={<Game ref={childRef} assistant_global={assistant_global} />} />
+          {/* <Route exact path="/theory" element={<Theory assistant_global={assistant_global} />} /> */}
 
-        <Route path="/theory/1" element={<PastTheory assistant_global={assistant_global} />} />
-        <Route path="/theory/2" element={<PresentTheory assistant_global={assistant_global} />} />
-        <Route path="/theory/3" element={<FutureTheory assistant_global={assistant_global} />} />
-      </Routes>
-    </div>
+          <Route path="/theory/1" element={<PastTheory assistant_global={assistant_global} />} />
+          <Route path="/theory/2" element={<PresentTheory assistant_global={assistant_global} />} />
+          <Route path="/theory/3" element={<FutureTheory assistant_global={assistant_global} />} />
+        </Routes>
+      </div>
   );
-
 }
 
 export default App;
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,9 +1,60 @@
+// import React from "react"
+// import { useNavigate } from 'react-router-dom';
+// import { Button } from "@salutejs/plasma-ui";
+// import { BodyL, bodyL } from '@salutejs/plasma-ui';
+// import {useDefaultSectionFocus, useSection} from '@salutejs/spatial';
+//
+//
+// function Menu({assistant_global}) {
+//
+//     const navigate = useNavigate();
+//     const [menuProps] = useSection('menu');
+//
+//     function toStartQuiz(type) {
+//         assistant_global(type, 'choose_theme')
+//     }
+//
+//     function showTheory(type) {
+//         assistant_global(type, 'choose_theory')
+//     }
+//
+//     useDefaultSectionFocus('menu');
+//
+//     return (
+//         <div className="Menu">
+//             <h1 className="quiz-text">Выбери время:</h1>
+//             <div {...menuProps}>
+//                 <div className="types">
+//                     <div style={{display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
+//                         <Button text="Past" size="s" onClick={() => toStartQuiz(1)}
+//                                 className="sn-section-item menu-btn" tabIndex={-1}/>
+//                         <Button text="Theory" size="s" view="secondary" onClick={() => showTheory(1)}
+//                                 className="sn-section-item theory-button" tabIndex={-1}/>
+//                     </div>
+//                     <div style={{display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
+//                         <Button text="Present" size="s" onClick={() => toStartQuiz(2)}
+//                                 className="sn-section-item menu-btn" tabIndex={-1}/>
+//                         <Button text="Theory" size="s" view="secondary" onClick={() => showTheory(2)}
+//                                 className="sn-section-item theory-button" tabIndex={-1}/>
+//                     </div>
+//                     <div style={{display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
+//                         <Button text="Future" size="s" onClick={() => toStartQuiz(3)} className="sn-section-item menu-btn" tabIndex={-1}/>
+//                         <Button text="Theory" size="s" view="secondary" onClick={() => showTheory(3)}
+//                                 className="sn-section-item theory-button" tabIndex={-1}/>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+//
+// export default Menu
+
 import React from "react"
 import { useNavigate } from 'react-router-dom';
 import { Button } from "@salutejs/plasma-ui";
 import { BodyL, bodyL } from '@salutejs/plasma-ui';
 import {useDefaultSectionFocus, useSection} from '@salutejs/spatial';
-
 
 function Menu({assistant_global}) {
 
@@ -11,11 +62,11 @@ function Menu({assistant_global}) {
     const [menuProps] = useSection('menu');
 
     function toStartQuiz(type) {
-        assistant_global(type, 'choose_theme')
+        assistant_global(type, "choose_theme")
     }
 
     function showTheory(type) {
-        assistant_global(type, 'choose_theory')
+        assistant_global(type, "choose_theory")
     }
 
     useDefaultSectionFocus('menu');
@@ -26,21 +77,31 @@ function Menu({assistant_global}) {
             <div {...menuProps}>
                 <div className="types">
                     <div style={{display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
+                        <span className="button-text"
+                              style={{marginRight: '10px', minWidth: '100px', padding: '0 30px'}}>Past tense:</span>
                         <Button text="Past" size="s" onClick={() => toStartQuiz(1)}
-                                className="sn-section-item menu-btn" tabIndex={-1}/>
+                                className="sn-section-item menu-btn" tabIndex={-1} style={{flexShrink: 0}}/>
                         <Button text="Theory" size="s" view="secondary" onClick={() => showTheory(1)}
-                                className="sn-section-item theory-button" tabIndex={-1}/>
+                                className="sn-section-item theory-button" tabIndex={-1}
+                                style={{marginLeft: '10px', flexShrink: 0}}/>
                     </div>
                     <div style={{display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
+                        <span className="button-text"
+                              style={{marginRight: '10px', minWidth: '100px'}}>Present tense:</span>
                         <Button text="Present" size="s" onClick={() => toStartQuiz(2)}
-                                className="sn-section-item menu-btn" tabIndex={-1}/>
+                                className="sn-section-item menu-btn" tabIndex={-1} style={{flexShrink: 0}}/>
                         <Button text="Theory" size="s" view="secondary" onClick={() => showTheory(2)}
-                                className="sn-section-item theory-button" tabIndex={-1}/>
+                                className="sn-section-item theory-button" tabIndex={-1}
+                                style={{marginLeft: '10px', flexShrink: 0}}/>
                     </div>
                     <div style={{display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
-                        <Button text="Future" size="s" onClick={() => toStartQuiz(3)} className="sn-section-item menu-btn" tabIndex={-1}/>
+                        <span className="button-text"
+                              style={{marginRight: '10px', minWidth: '100px', padding: '0 12px'}}>Future tense: </span>
+                        <Button text="Future" size="s" onClick={() => toStartQuiz(3)}
+                                className="sn-section-item menu-btn" tabIndex={-1} style={{flexShrink: 0}}/>
                         <Button text="Theory" size="s" view="secondary" onClick={() => showTheory(3)}
-                                className="sn-section-item theory-button" tabIndex={-1}/>
+                                className="sn-section-item theory-button" tabIndex={-1}
+                                style={{marginLeft: '10px', flexShrink: 0}}/>
                     </div>
                 </div>
             </div>
